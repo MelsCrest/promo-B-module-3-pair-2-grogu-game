9c72d0f5cc9f52f0dc89718531bb45672de6a886
MelsCrest <melisacresta@gmail.com>
2025-01-22 11:25:24 +0100

    react template and migration done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,47 @@
-import '../styles/App.css' 
+import '../styles/App.scss' 
 
 function App() { 
   return (
-      <div>
-        <h1>Template react</h1>
-      </div>
+      <>
+      <header>
+        <h1>¡Cuidado con Grogu!</h1>
+      </header>
+      <main className="page">
+        <section className="board">
+          <div className="cell"><div className="grogu">👣</div></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+        </section>
+
+        <section>
+          <button className="dice">Lanzar Dado</button>
+          <div className="game-status">En curso</div>
+        </section>
+
+        <section className="goods-container">
+          <div className="goods-item">🍪</div>
+          <div className="goods-item">🍪</div>
+          <div className="goods-item">🍪</div>
+        </section>
+        <section className="goods-container">
+          <div className="goods-item">🥚</div>
+          <div className="goods-item">🥚</div>
+          <div className="goods-item">🥚</div>
+        </section>
+        <section className="goods-container">
+          <div className="goods-item">🐸</div>
+          <div className="goods-item">🐸</div>
+          <div className="goods-item">🐸</div>
+        </section>
+        <section>
+          <button className="restart-button">Reiniciar Juego</button>
+        </section>
+      </main>
+      </>
   );
 }
 
